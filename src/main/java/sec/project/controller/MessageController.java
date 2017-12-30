ae14c50708937f3281cfda39bbb1d9d683d840f5
Mikael Laine <hkmlai@utu.fi>
2017-12-30 15:21:34 +0200

    Add insecure session cookie and links to templates

--- src/main/java/sec/project/controller/MessageController.java
+++ src/main/java/sec/project/controller/MessageController.java
@@ -1,5 +1,8 @@
 package sec.project.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,13 +24,23 @@ public class MessageController {
     }
     
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String loadMessageboard(Model model) {
+    public String loadMessageboard(Model model, HttpServletRequest request, HttpServletResponse response) {
+        // A completely stupid thing to do just to prove a point about the
+        // importance of isHttpOnly for session cookies.
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("JSESSIONID")) {
+                    cookie.setHttpOnly(false);
+                    response.addCookie(cookie);
+                }
+            }
+        }
         model.addAttribute("comments", messageRepository.findAll());
         return "messageboard";
     }
     
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String msg) {
+    public String submitForm(@RequestParam(value="name",required=true) String name, @RequestParam(value="msg",required=true) String msg) {
         messageRepository.save(new Message(name, msg));
         return "redirect:/";
     }
